3079b2de9b1bc6ef7f20e0491a65ced956d7c1b1
mohamdfakir <mohamadjaid786@gmail.com>
2022-11-27 20:53:23 +0530

    added Employee wages problems

---
+++ src/day4problems/EmployeePresentOrAbsent.java
@@ -0,0 +1,22 @@
+package day4problems;
+
+    public class EmployeePresentOrAbsent {
+        public static void main(String[] args) {
+
+            //Declaring the variables.
+            double isPresent = 1;
+            double randomCheck = (double)Math.floor((Math.random()*10)%2);
+
+            //Checking if the Employee is present!!!
+            if (randomCheck == isPresent)
+            {
+                System.out.println("Employee is present");
+            }
+            else
+            {
+                System.out.println("Employee is absent");
+            }
+        }
+    }
+
+
